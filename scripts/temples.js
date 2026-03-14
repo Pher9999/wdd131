dd1ed1bc363ea00a53ed6cd2528c090c3ffa9136
pher9999 <criordan@hal-9000.net>
2026-03-14 15:00:28 -0500

    fixes for color

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,9 @@
-/* hamburguer button */
+const today = new Date();
+
+document.getElementById("currentyear").innerHTML = `©${today.getFullYear()}`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+/* hamburger button */
 const menuButton = document.getElementById("menu-button");
 const navMenu = document.getElementById("nav-menu");
 
