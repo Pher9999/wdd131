0994578650b2508e6bbfb08515cdb8bf2a781a0e
pher9999 <criordan@hal-9000.net>
2026-03-14 15:03:20 -0500

    fixed footer

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,7 @@
 const today = new Date();
 
 document.getElementById("currentyear").innerHTML = `©${today.getFullYear()}`;
-document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+document.getElementById("lastmodified").innerHTML = `Last Modified: ${document.lastModified}`;
 
 /* hamburger button */
 const menuButton = document.getElementById("menu-button");
