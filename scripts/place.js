e915928f7c6071270101d9c9b04454b72c92b1a3
pher9999 <criordan@hal-9000.net>
2026-03-21 17:20:58 -0500

    places page basic finish

--- scripts/place.js
+++ scripts/place.js
@@ -2,6 +2,7 @@ const today = new Date();
 
 document.getElementById("currentyear").innerHTML = `©${today.getFullYear()}`;
 document.getElementById("lastmodified").innerHTML = `Last Modified: ${document.lastModified}`;
+document.getElementById("windChill").innerHTML = `${calculateWindChill(74, 3)}`;
 
 /* hamburger button */
 const menuButton = document.getElementById("menu-button");
@@ -17,3 +18,15 @@ menuButton.addEventListener("click", () => {
         menuButton.innerHTML = "☰";
     }
 });
+
+const temperature = 74;
+const windSpeed = 3;
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 50 && windSpeed >= 3) {
+        const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+        return Math.round(windChill);
+    } else {
+        return "N/A";
+    }
+}
